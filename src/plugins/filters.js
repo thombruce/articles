a9d52845aee0d4e08f31caf393393e5bfa73f2b4
Thom Bruce <thom@thombruce.com>
2021-01-08 05:07:50 +0000

    map articles in index

--- src/plugins/filters.js
+++ src/plugins/filters.js
@@ -1,3 +1,12 @@
 import Vue from 'vue'
 
 Vue.filter('formatDate', val => new Date(val).toLocaleDateString())
+
+Vue.filter('textPreview', val => {
+  const doc = new DOMParser().parseFromString(val, 'text/html')
+  if (doc.body.children[0]) {
+    return doc.body.children[0].textContent || 'Untitled Note'
+  } else {
+    return 'Untitled Note'
+  }
+})
--- src/views/articles/Index.vue
+++ src/views/articles/Index.vue
@@ -23,41 +23,32 @@
             link
           )
             VListItemContent
-              strong {{ htmlToString(article.content) }}
+              strong {{ article.content | textPreview }}
               div
                 time(:datetime="article.updatedAt") {{ article.updatedAt | formatDate }}
-                small.grey--text(v-if="article.updatedAt !== article.createdAt") (Edited)
 
     VContainer.col-8.offset-4.col-md-9.offset-md-3.col-lg-10.offset-lg-2
       RouterView
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
-  data () {
-    return {
-      articles: null
-    }
+  computed: {
+    ...mapGetters('articles', {
+      articles: 'all'
+    })
   },
 
   methods: {
     ...mapActions('articles', [
       'index'
-    ]),
-    htmlToString (htmlString) {
-      const doc = new DOMParser().parseFromString(htmlString, 'text/html')
-      if (doc.body.children[0]) {
-        return doc.body.children[0].textContent || 'Untitled Note'
-      } else {
-        return 'Untitled Note'
-      }
-    }
+    ])
   },
 
-  async mounted () {
-    this.articles = await this.index()
+  mounted () {
+    this.index()
   }
 }
 </script>
